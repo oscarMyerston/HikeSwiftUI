18cce87e44c2f7d522d4e636da5ab816f77c1f38
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-05-29 21:55:39 -0500

    Initial Commit

---
+++ HikeSwiftUI/HikeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeSwiftUIApp.swift
+//  HikeSwiftUI
+//
+//  Created by Oscar David Myerston Vega on 29/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
