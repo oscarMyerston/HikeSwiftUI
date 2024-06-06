3ce747dd4d55a5e4a5ab51be63ea50baed6d8521
Oscar David Myerston Vega <oscar.myerston@wiedii.co>
2024-06-05 21:42:41 -0500

    Add Header Component

--- HikeSwiftUI/Component/CardView.swift
+++ HikeSwiftUI/Component/CardView.swift
@@ -11,21 +11,61 @@ struct CardView: View {
     var body: some View {
         ZStack {
             CustomBackgroundView()
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(colors: [
-                            Color("ColorIndigoMedium"),
-                            Color("ColorSalmonLight")
-                        ],
-                                       startPoint: .topLeading, endPoint: .bottomTrailing)
-                    )
-                    .frame(width: 256, height: 256)
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
-            }
-        } // Card
+
+            VStack {
+                // MARK: - HEADER
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking ")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [
+                                        .customGrayLight,
+                                        .customGrayMedium
+                                    ],
+                                    startPoint: .top,
+                                    endPoint: .bottom)
+                        )
+                        Spacer()
+                        // Button Design
+                        Button(action: {
+                            // ACTION: Show a Sheet
+                            print("The button was pressed")
+                        }, label: {
+                            CustomButtonView()
+                        })
+                    }
+
+
+                    Text("Fun and enjoyable outdoor activity for friends and families")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+
+                }//: HEADER
+                .padding(.horizontal, 30)
+
+                // MARK: - Main Content
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(colors: [
+                                Color("ColorIndigoMedium"),
+                                Color("ColorSalmonLight")
+                            ],
+                                           startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                // MARK: - FOOTER
+
+            }//: VSTACK
+        } //: Card
         .frame(width: 320, height: 570)
     }
 }
